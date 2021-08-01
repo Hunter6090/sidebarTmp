485fdc1c9d2f7d963b26755fa45865a279fd20af
HUN7 <rockofsky1@gmail.com>
2021-08-02 00:34:09 +0800

    2021-08-01: 1.Base template

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} /> //Need add 'exact' to switch component of display.
+          <Route path='/products' exact component={Products} />
+          <Route path='/reports' exact component={Reports} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
