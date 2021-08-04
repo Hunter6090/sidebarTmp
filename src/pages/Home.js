e1131fe9f731ff40e4d788cf391e49b938d5d68b
HUN7 <rockofsky1@gmail.com>
2021-08-04 23:04:57 +0800

    2021-08-04: 1.Apply react-bootstrap.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Home(){
 	return(
 		<div className= 'home'>
-			<h1>Home</h1>
+			<Container>
+                <Row>
+                    <Col md={2}>Col={2}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                    <Col md={1}>Col={1}</Col>
+                </Row>
+            </Container>
 		</div>
 	)
 }
