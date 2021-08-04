241d6c76cf41c3b90552a1516d1d9e237b84253d
HUN7 <rockofsky1@gmail.com>
2021-08-05 00:55:30 +0800

    2021-08-04: 1.Add css animation.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,36 @@
 import React from 'react';
-
+import './styles.css';
 import { Container, Row, Col } from 'react-bootstrap';
+
+import Pic1 from "../img/pic1.jpg";
+import Pic2 from "../img/pic2.jpg";
 
 function Home(){
 	return(
 		<div className= 'home'>
 			<Container>
-                <Row>
-                    <Col md={2}>Col={2}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
-                    <Col md={1}>Col={1}</Col>
+                <Row className= 'header'>
+                    <Col>
+                    	<div className= 'headerText'>Title</div>
+                    </Col>
+                </Row>
+                <Row className= 'contentLayout'>
+                	<Col md={6}>
+                		<div>
+                			Maybe you wanna add some info here :)
+                		</div>
+                	</Col>
+                	<Col md={6}>
+                		<div className='rightContentLayout'>
+                			Here you can do some description to the figure
+	                		<div>
+	                			<img className= 'pic1Layout' src ={Pic1} />
+	                		</div>
+	                		<div>
+	                			<img className= 'pic2Layout' src ={Pic2} />
+	                		</div>
+                		</div>
+                	</Col>
                 </Row>
             </Container>
 		</div>
